5dbdd072a57289bfd0a30729582ef0f309b4892b
Leighton8807 <joge.8807@gmail.com>
2023-10-22 18:36:08 -0500

    Add: se agrega shoppingCart

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+
+export const Footer = () => {
+
+  return (
+    <>
+    <footer>
+        <p>Elaborado por: Maria Pedraza y Jorge Leighton</p>
+         <p>&copy; 2023 ADSO 2560321-2
+         </p>
+    </footer>
+    </>
+  );
+};
